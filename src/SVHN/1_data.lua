e7c4660a2c53269929b73388462755a8b5791e06
gyoho <yaopeng.wu@sjsu.edu>
2016-03-18 00:30:46 -0700

    working on normalization

--- src/SVHN/1_data.lua
+++ src/SVHN/1_data.lua
@@ -131,7 +131,59 @@ print '==> preprocessing data'
 trainData.data = trainData.data:float()
 testData.data = testData.data:float()
 
+-- Convert all images to YUV
+print '==> preprocessing data: colorspace RGB -> YUV'
+for i = 1,trainData:size() do
+   trainData.data[i] = image.rgb2yuv(trainData.data[i])
+end
+for i = 1,testData:size() do
+   testData.data[i] = image.rgb2yuv(testData.data[i])
+end
 
+-- Name channels for convenience
+channels = {'y','u','v'}
+
+-- Normalize each channel, and store mean/std
+-- per channel. These values are important, as they are part of
+-- the trainable parameters. At test time, test data will be normalized
+-- using these values.
+print '==> preprocessing data: normalize each feature (channel) globally'
+mean = {}
+std = {}
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   mean[i] = trainData.data[{ {},i,{},{} }]:mean()
+   std[i] = trainData.data[{ {},i,{},{} }]:std()
+   trainData.data[{ {},i,{},{} }]:add(-mean[i])
+   trainData.data[{ {},i,{},{} }]:div(std[i])
+end
+
+-- Normalize test data, using the training means/stds
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   testData.data[{ {},i,{},{} }]:add(-mean[i])
+   testData.data[{ {},i,{},{} }]:div(std[i])
+end
+
+-- Local normalization
+print '==> preprocessing data: normalize all three channels locally'
+
+-- Define the normalization neighborhood:
+neighborhood = image.gaussian1D(13)
+
+-- Define our local normalization operator (It is an actual nn module, 
+-- which could be inserted into a trainable model):
+normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1):float()
+
+-- Normalize all channels locally:
+for c in ipairs(channels) do
+   for i = 1,trainData:size() do
+      trainData.data[{ i,{c},{},{} }] = normalization:forward(trainData.data[{ i,{c},{},{} }])
+   end
+   for i = 1,testData:size() do
+      testData.data[{ i,{c},{},{} }] = normalization:forward(testData.data[{ i,{c},{},{} }])
+   end
+end
 ----------------------------------------------------------------------
 print '==> verify statistics'
 
