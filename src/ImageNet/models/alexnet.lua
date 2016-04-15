d263c0e22d72374314aec04905d02cbe250df68e
Yaopeng Wu <gyoho@outlook.com>
2016-04-14 20:38:05 -0700

    Update alexnet.lua

--- src/ImageNet/models/alexnet.lua
+++ src/ImageNet/models/alexnet.lua
@@ -1,12 +1,19 @@
 function createModel(nGPU)
    local features = nn.Concat(2)
-   local fb1 = nn.Sequential() -- branch 1
+   local fb1 = nn.Sequential()                              -- Create 1st Network
+   
+                                                            -- goes in -> goes out
+   -- 1st convolution layer
    fb1:add(nn.SpatialConvolution(3,48,11,11,4,4,2,2))       -- 224 -> 55
    fb1:add(nn.ReLU(true))
    fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
+   
+   -- 2nd convolution layer
    fb1:add(nn.SpatialConvolution(48,128,5,5,1,1,2,2))       --  27 -> 27
    fb1:add(nn.ReLU(true))
    fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
+   
+   -- 3rd convolution layer
    fb1:add(nn.SpatialConvolution(128,192,3,3,1,1,1,1))      --  13 ->  13
    fb1:add(nn.ReLU(true))
    fb1:add(nn.SpatialConvolution(192,192,3,3,1,1,1,1))      --  13 ->  13
@@ -14,6 +21,7 @@ function createModel(nGPU)
    fb1:add(nn.SpatialConvolution(192,128,3,3,1,1,1,1))      --  13 ->  13
    fb1:add(nn.ReLU(true))
    fb1:add(nn.SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
+
 
    local fb2 = fb1:clone() -- branch 2
    for k,v in ipairs(fb2:findModules('nn.SpatialConvolution')) do
